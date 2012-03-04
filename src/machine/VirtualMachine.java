cd46aba5b8c4d5c331a3c54b53fe621d93d8d164
Zygimantas Gatelis <zygis.gg@gmail.com>
2012-03-04 22:04:11 +0200

    Initialized VirtualMachine

--- src/machine/VirtualMachine.java
+++ src/machine/VirtualMachine.java
@@ -2,4 +2,15 @@ package machine;
 
 public class VirtualMachine {
 
+	
+	private VirtualMachineRegisters registers;
+	
+	public VirtualMachine(VirtualMachineRegisters registers) {
+		this.registers = registers;
+		
+	}
+	
+	public VirtualMachineRegisters getRegisters() {
+		return this.registers;
+	}
 }
