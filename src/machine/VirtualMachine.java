3b64b1c6f2942a7fdf088d5638bbe65c1b3ef798
Zygimantas Gatelis <zygis.gg@gmail.com>
2012-03-04 22:43:50 +0200

    memory field added, a few constants creates

--- src/machine/VirtualMachine.java
+++ src/machine/VirtualMachine.java
@@ -1,12 +1,17 @@
 package machine;
 
 public class VirtualMachine {
-
+	
+	public static final int VIRTUAL_MEMORY_SIZE = 0xff;
+	private static final int WORD_SIZE = 4;
 	
 	private VirtualMachineRegisters registers;
+	//ToDo memory ... Class or RealMachine field ?
+	private char[][] memory;
 	
 	public VirtualMachine(VirtualMachineRegisters registers) {
 		this.registers = registers;
+		this.memory = new char[VIRTUAL_MEMORY_SIZE][WORD_SIZE];
 		
 	}
 	
@@ -14,5 +19,9 @@ public class VirtualMachine {
 		return this.registers;
 	}
 	
+	public char[] getWord(int index) {
+		
+		return memory[index];
+	}
 	
 }
