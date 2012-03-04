13fdf399c0515429566f017c06d325f2316010b6
Povilas Dauksas <povilas.d@gmail.com>
2012-03-05 00:09:30 +0200

    Some Real Machine class updates

--- src/machine/Realmachine.java
+++ src/machine/Realmachine.java
@@ -6,6 +6,23 @@ public class Realmachine {
 	 * shows from where starts code segment in virtual memory
 	 */
 	public static final int INSTRUCTON_COUNTER_START = 0x81; // not sure if hex OK
+	public static final int REAL_MEMORY_SIZE = 0xfff;
+	private static final int WORD_SIZE = 4;
+	private char[][] memory;
+	private VirtualMachine activeVirtualMachine;
+	private RealMachineRegisters registers;
 	
+	public Realmachine() {
+		this.memory = new char[REAL_MEMORY_SIZE][WORD_SIZE];
+		this.activeVirtualMachine = null;
+		this.registers = new RealMachineRegisters();
+	}
 	
+	public void setActiveVirtualMachine(VirtualMachine machine) {
+		this.activeVirtualMachine = machine;
+	}
+	
+	public char[] getWord(int index) {
+		return memory[index];
+	}
 }
