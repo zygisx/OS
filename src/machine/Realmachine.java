02e4195a1f83feaeb7062eed4a0cc3cc4c602fa0
Zygimantas Gatelis <zygis.gg@gmail.com>
2012-03-06 21:01:54 +0200

    Everything set to static.

--- src/machine/Realmachine.java
+++ src/machine/Realmachine.java
@@ -8,14 +8,14 @@ public class Realmachine {
 	public static final int CODE_SEGMENT_START = 0x81; // not sure if hex OK
 	public static final int REAL_MEMORY_SIZE = 0xfff;
 	private static final int WORD_SIZE = 4;
-	private char[][] memory;
-	private VirtualMachine activeVirtualMachine;
-	private RealMachineRegisters registers;
+	private static char[][] memory;
+	private static VirtualMachine activeVirtualMachine;
+	private static RealMachineRegisters registers;
 	
-	public Realmachine() {
-		this.memory = new char[REAL_MEMORY_SIZE][WORD_SIZE];
-		this.activeVirtualMachine = null;
-		this.registers = new RealMachineRegisters();
+	static {
+		memory = new char[REAL_MEMORY_SIZE][WORD_SIZE];
+		activeVirtualMachine = null;
+		registers = new RealMachineRegisters();
 	}
 	
 	public void setActiveVirtualMachine(VirtualMachine machine) {
