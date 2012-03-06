be26d62e59e039916017d3553fb6f38ad9a9ddb2
Zygimantas Gatelis <zygis.gg@gmail.com>
2012-03-06 21:15:18 +0200

    Added abstract register class

---
+++ src/machine/Register.java
@@ -0,0 +1,16 @@
+package machine;
+
+public abstract class Register {
+
+	private char[] value;
+
+	public char[] getValue() {
+		return value;
+	}
+
+	public void setValue(char[] value) {
+		this.value = value;
+	}
+	
+	
+}
