da87419ca0dd1ecdebb75fe46da9be9357223413
Zygimantas Gatelis <zygis.gg@gmail.com>
2012-03-07 16:01:14 +0200

    Added test to real machine class

---
+++ src/test/RealMachineTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import machine.*;
+
+import org.junit.Test;
+
+public class RealMachineTest {
+
+	@Test
+	public void testGetWord() {
+		// all memory bytes set to 0
+		Word w = new Word("0000");
+		assertEquals(w.getStringValue(), Realmachine.getWord(69).getStringValue());
+		
+	}
+
+}
