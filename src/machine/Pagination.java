a0660769fd5991c32d8cff4eee44af2dd9250ad3
Zygimantas Gatelis <zygis.gg@gmail.com>
2012-03-07 20:15:22 +0200

    Shit happens. Finally getRealAddress method created. Need testing

--- src/machine/Pagination.java
+++ src/machine/Pagination.java
@@ -1,10 +1,23 @@
 package machine;
 
+
 public class Pagination {
 	
+	private Word[] table;
 	
-	public Pagination(Word[] paginationTable) {
-		
+	public Pagination(Word[] table) {
+		this.table = table;
 	}
+	
+	/* return real address */
+	public int getRealAddress(byte virtualAddress) {
+		byte plr = RealMachineRegisters.getPLR();
+		int block = virtualAddress / 0x10;
+		int offset = virtualAddress % 0x10;
+		int realMemoryblock = Realmachine.getWord(plr+block).getHexValue(); // returns real memory block number
+		return realMemoryblock*0x10 + offset;
+	}
+	
+	
 
 }
