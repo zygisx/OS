dcb35f4389fa912359e67e55338f43eb9e00d065
Zygimantas Gatelis <zygis.gg@gmail.com>
2012-03-12 22:27:13 +0200

    Reg Exp tested

--- src/test/ParserTest.java
+++ src/test/ParserTest.java
@@ -14,9 +14,14 @@ public class ParserTest {
 		assertFalse(s.matches("\\$\\d{2}:\\$"));
 		s = "$0:$";
 		assertFalse(s.matches("\\$\\d{2}:\\$"));
-		s = "$69:$";
-		assertTrue(s.matches("\\$\\d{2}:\\$"));
-		
+		s = "$69:$ ";
+		assertTrue(s.matches("\\$[0-9A-Fa-f]{2}:\\$.*$"));
+		s = "$6f:$   \t ";
+		assertTrue(s.matches("\\$[0-9A-Fa-f]{2}:\\$.*$"));
+		s = "$6f:$   \t";
+		assertTrue(s.matches("\\$[0-9A-Fa-f]{2}:\\$.*$"));
+		s = "0$6f:$   \t";
+		assertFalse(s.matches("\\$[0-9A-Fa-f]{2}:\\$.*$"));
 	}
 
 }
