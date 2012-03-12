6078c056c78bf61a95b8c58ab8dd5b2a897f71b8
Povilas Dauksas <povilas.d@gmail.com>
2012-03-13 01:10:40 +0200

    Switch - done

--- src/machine/VirtualMachine.java
+++ src/machine/VirtualMachine.java
@@ -22,6 +22,67 @@ public class VirtualMachine {
 	}
 	
 	
-	
+	public void processCommand(String command) {
+		String commandPart;
+		byte address = 0;
+		commandPart = command.substring(0, 1);
+		if (command.length() >= 4) {
+			address = Byte.parseByte(command.substring(2,3));
+		}
+		
+		switch (commandPart) {
+		case "AD":
+			Realmachine.AD(address);
+		break;
+		case "SB":
+			Realmachine.SB(address);
+		break;
+		case "ML":
+			Realmachine.ML(address);
+		break;
+		case "DV":
+			Realmachine.DV(address);
+		break;
+		case "CP":
+			Realmachine.CP();
+		break;
+		case "CS":
+			Realmachine.CS();
+		break;
+		case "L1":
+			Realmachine.L1(address);
+		break;
+		case "L2":
+			Realmachine.L2(address);
+		break;
+		case "S1":
+			Realmachine.S1(address);
+		break;
+		case "S2":
+			Realmachine.S2(address);
+		break;
+		case "SI":
+			Realmachine.SI(address);
+		break;
+		case "JP":
+			Realmachine.JP(address);
+		break;
+		case "JE":
+			Realmachine.JE(address);
+		break;
+		case "JM":
+			Realmachine.JM(address);
+		break;
+		case "JL":
+			Realmachine.JL(address);
+		break;
+		case "JF":
+			Realmachine.JF(address);
+		break;
+		case "JS":
+			Realmachine.JS(address);
+		break;
+		}
+	}
 	
 }
