53e03c17413e5de986dbbc05c273b34e7591380d
Zygimantas Gatelis <zygis.gg@gmail.com>
2012-03-17 15:07:39 +0200

    added exceptions

---
+++ src/exception/BadFileException.java
@@ -0,0 +1,14 @@
+package exception;
+
+public class BadFileException extends OSException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public BadFileException(String message) {
+		super(message);
+	}
+
+}
---
+++ src/exception/OSException.java
@@ -0,0 +1,16 @@
+package exception;
+
+public class OSException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	public OSException(String message) {
+		super(message);
+	}
+
+	
+}
