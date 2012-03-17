0bc0d2b0d78e4a117350aaa996d2036e00a4065a
Zygimantas Gatelis <zygis.gg@gmail.com>
2012-03-17 14:34:53 +0200

    simplified input.

--- src/gui/ConsolePanel.java
+++ src/gui/ConsolePanel.java
@@ -10,10 +10,12 @@ import javax.swing.JTextArea;
 import java.awt.Color;
 import javax.swing.JTextField;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class ConsolePanel extends JPanel implements KeyListener {
+public class ConsolePanel extends JPanel {
 
 	private JTextArea console;
 	private MainFrame parent;
@@ -21,64 +23,24 @@ public class ConsolePanel extends JPanel implements KeyListener {
 	
 	public ConsolePanel(MainFrame parent) {
 		this.parent = parent;
-		setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
 		
-		JTextArea console = new JTextArea();
+		console = new JTextArea();
+		console.setEditable(false);
 		console.setLineWrap(true);
-		console.setRows(15);
+		console.setRows(10);
 		console.setColumns(30);
 		console.setWrapStyleWord(true);
-		console.setForeground(Color.MAGENTA);
+		console.setForeground(Color.WHITE);
 		console.setBackground(Color.BLACK);
 		console.setFont(new Font("Monospaced",Font.PLAIN,15));
 		
-		KeyListener keyListener = new KeyListener() {
-		      public void keyPressed(KeyEvent keyEvent) {
-		        printIt("Pressed", keyEvent);
-		      }
-
-		      public void keyReleased(KeyEvent keyEvent) {
-		        printIt("Released", keyEvent);
-		      }
-
-		      public void keyTyped(KeyEvent keyEvent) {
-		        printIt("Typed", keyEvent);
-		      }
-
-		      private void printIt(String title, KeyEvent keyEvent) {
-		        int keyCode = keyEvent.getKeyCode();
-		        String keyText = KeyEvent.getKeyText(keyCode);
-		        System.out.println(title + " : " + keyText + " / " + keyEvent.getKeyChar());
-		      }
-		    };
-		console.addKeyListener(this);
-		console.setText(">Welcome.");
-		add(console);
-		
-		JTextPane textPane = new JTextPane();
-		textPane.setPreferredSize(new Dimension(53, 100));
-		
-		add(textPane);
+		setLayout(new BorderLayout(5, 10));
+		console.setText(">Welcome.\n");
+		add(console, BorderLayout.CENTER);
 
 	}
-	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void keyTyped(KeyEvent e) {
 	
-		if (e.getKeyChar() == KeyEvent.VK_ENTER) {
-			this.parent.inputAccepted(this.input);
-	        System.out.println("ENTER pressed");
-	    }
-		else this.input += e.getKeyChar();
+	public void output(String text) {
+		console.append(">" + text);
 	}
-
 }
