001a981b0b79f872a2d4bcfa35503a185a78aef8
Zygimantas Gatelis <zygis.gg@gmail.com>
2012-05-15 00:26:28 +0300

    added consts

--- src/os/Constants.java
+++ src/os/Constants.java
@@ -4,4 +4,7 @@ public final class Constants {
 
 	
 	public static final int MAX_PRIORITY = 100;
+	public static final int MIN_PRIORITY = 1;
+	public static final int USER_PROCESS_PRIORITY = 10; // if all user processes has the same priority. 
+	
 }
