7507dec64d1c2ed133a9107fa1c803ee8c90711d
Zygimantas Gatelis <zygis.gg@gmail.com>
2012-05-15 13:05:15 +0300

    Added resource class

---
+++ src/os/Resource.java
@@ -0,0 +1,28 @@
+package os;
+
+import java.util.ArrayList;
+
+public class Resource {
+
+	
+	private String id;
+	private boolean isReusable;
+	private int parenProcess; // don't know why int..?
+	private ArrayList<processes.Process> waitingForResourceProcessList;
+	
+	// nezinau kas cia tokie
+	// nuoroda á prieinamumo sàraðà
+	// nuoroda á paskirstytojo objektà
+
+	
+	public Resource(String id, int parent) {
+		this.id = id;
+		this.parenProcess = parent;
+		this.waitingForResourceProcessList = new ArrayList<processes.Process>();
+	}
+	
+	
+	
+	
+	
+}
