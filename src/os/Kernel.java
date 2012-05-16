38fb8ba30b1e52a35fa8c17feb88f9e23077cfb5
Zygimantas Gatelis <zygis.gg@gmail.com>
2012-05-16 21:44:06 +0300

    Added Kernel class

---
+++ src/os/Kernel.java
@@ -0,0 +1,21 @@
+package os;
+
+/**
+ * main class for main loop
+ * 
+ * @author 
+ *
+ */
+public class Kernel {
+
+	// I guess this class should work like RealMachine in previous project.
+	// It holds all os information, processes lists taskManeger resources list.
+	// Here should be main loop for running os. 
+	
+	
+	public Kernel() {
+		
+	}
+	
+	
+}
