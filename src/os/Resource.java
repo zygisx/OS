1e888b029fff8c160bde21ead924ce5335d73421
Zygimantas Gatelis <zygis.gg@gmail.com>
2012-05-17 18:13:36 +0300

    added isAvailable field

--- src/os/Resource.java
+++ src/os/Resource.java
@@ -6,7 +6,8 @@ public class Resource {
 
 	
 	private String id;
-	private boolean isReusable;
+	private boolean isReusable; // kas èia ?
+	private boolean isAvailable;
 	private int parenProcess; // don't know why int..?
 	private ArrayList<processes.Process> waitingForResourceProcessList;
 	
@@ -19,6 +20,19 @@ public class Resource {
 		this.id = id;
 		this.parenProcess = parent;
 		this.waitingForResourceProcessList = new ArrayList<processes.Process>();
+		this.isAvailable = true;
+	}
+	
+	public void free() {
+		this.isAvailable = true;
+	}
+	
+	public void occupy() {
+		this.isAvailable = false;
+	}
+	
+	public boolean isAvailable() {
+		return this.isAvailable;
 	}
 
 	public String getId() {
