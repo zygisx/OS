4018c2d6b1595dce044c0dda7e6a146f56844784
Zygimantas Gatelis <zygis.gg@gmail.com>
2012-05-18 14:21:24 +0300

    minimal implementation of startstrop process

--- src/processes/StartStop.java
+++ src/processes/StartStop.java
@@ -1,6 +1,8 @@
 package processes;
 
 import os.Constants;
+import os.Kernel;
+import os.Resource;
 
 public class StartStop extends Process {
 
@@ -8,24 +10,58 @@ public class StartStop extends Process {
 	
 	public StartStop(String id) {
 		super(id, null, Constants.MAX_PRIORITY);  // start/stop has no parents. 
-		super.isSupervisorMode = true;	// true defines that it is system process
+		this.isSupervisorMode = true;	// true defines that it is system process
+		this.status = Status.READY;
+		this.missingResource = "";
 	}
 	
 	
 	@Override
 	public void run() {
 		
-		// system resources initialization
+		/*
+		 * switch to now in wich stage of process we are, in first stage sprocess do not require any resource
+		 * in second one it requires mosworkend resource
+		 */
 		
-		// initialize static resources
+		switch (this.missingResource) {
 		
-		// system processes initialization
+			case "":
+				// system resources initialization
+				this.createResources(); 
+				
+				// system processes initialization
+				this.createProcesses();
+				
+				// block until MOS finishes its work	
+				this.missingResource = "mosworkend";
+				return;
+			
+			case "mosworkend":
+				this.missingResource = ""; // no resource required 
+				
+				// destroy 
+				
+				// destroy
+				
+				return;
+			
+		}
+	}
+	
+	
+	public void createResources() {
+		Kernel.getResources().create(new Resource("processor", this.id));
+		// and so on 
+	}
+	
+	public void createProcesses() {
+		Kernel.createProcess(new Read("read", this.id, Constants.MAX_PRIORITY - 1));
+		Kernel.createProcess(new Read("chan3device", this.id, Constants.MAX_PRIORITY - 2));
+		Kernel.createProcess(new JCL("jcl", this.id, Constants.MAX_PRIORITY - 3));
+		Kernel.createProcess(new Loader("loader", this.id, Constants.MAX_PRIORITY - 4));
 		
-		
-		// block until MOS finishes its work	
-		
-		
-		// destroy processes & resources
+		// and others
 	}
 
 
