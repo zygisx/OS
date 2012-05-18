5c12185db4f8dd327b785e2297cb28378c814b7c
Zygimantas Gatelis <zygis.gg@gmail.com>
2012-05-18 23:18:29 +0300

    added setter for read process

--- src/processes/Read.java
+++ src/processes/Read.java
@@ -3,11 +3,17 @@ package processes;
 public class Read extends Process {
 	
 	
+	private String filename;
+	
 	public Read(String id, String parent, int priority) {
 		super(id, parent, priority);
 		this.isSupervisorMode = true;	// true defines that it is system process
 		this.status = Status.BLOCKED;
 		this.missingResource = "inputsream"; //TODO fix resource name. It don't correspond purpose
+	}
+	
+	public void setFileName(String fileName) {
+		this.filename = fileName;
 	}
 	
 	@Override
@@ -17,7 +23,8 @@ public class Read extends Process {
 			/*
 			case inputStream:
 				
-			case supervizorinës atminties resursas
+			case supervizorinës atminties resursas:
+			
 			
 			
 			Klausimas??? Kaip jam paduoti kokius failus skaityt, per resursa ? Ar kaþkaip ?
