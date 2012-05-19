26f38fa58d71d8bd42d1c256654ce49717f1ac36
Zygimantas Gatelis <zygis.gg@gmail.com>
2012-05-19 16:28:19 +0300

    added loadData & loadCode methods

--- src/machine/Parser.java
+++ src/machine/Parser.java
@@ -14,6 +14,9 @@ public class Parser {
 	private Parser() {		
 	}
 	
+	/**
+	 * DEPRECATED
+	 */
 	public static Word[] load(String fileName, Word[] memory) 
 			throws BadFileException {
 		
@@ -181,8 +184,59 @@ public class Parser {
 				data, code
 		};
 		return result;
-			
-
+	}
+	
+	public static void loadData(String data, Word[] mem) {
+		BufferedReader bf = new BufferedReader(new StringReader(data));
+		try {
+			String line;
+			int cursor = 0;
+			// read data segment
+			while ( (line = bf.readLine() )!= null) {
+				if (line.matches("\\$[0-9A-Fa-f]{2}:\\$.*$")) {
+					cursor = Integer.parseInt(line.substring(1, 3), 16);
+					continue;
+				}
+				else if (line.startsWith("DW") || line.startsWith("dw")) { //TODO decimal! operates only hex values
+					String s = line.substring(3);
+					if (s.length() > 4)
+						s = s.substring(0, 4);
+					//System.out.println(Integer.parseInt(s, 16) + "  " + cursor);
+					mem[cursor].setWordHexInt(Integer.parseInt(line, 16));
+				}
+				else if (line.startsWith("DB") || line.startsWith("db")) {
+					String s = line.substring(3);
+					if (s.length() > 4)
+						s = s.substring(0, 4);
+					//s = s.replaceAll("\\s+", "");
+					mem[cursor].setWordString(s);
+				}
+				else
+					continue;
+				cursor++;
+			}
+		}
+		catch (IOException ex) {
+			//TODO
+		}
+	}
+	
+	public static void loadCode(String code, Word[] mem) {
+		BufferedReader bf = new BufferedReader(new StringReader(code));
+		int cursor = 0x80;
+		try {
+			String line;
+			while ( (line = bf.readLine() )!= null) {
+				
+				if (line.length() < 4)
+					mem[cursor++].setWordString(String.format("%1$-4s", line));
+				else
+					mem[cursor++].setWordString(line.substring(0, 4));
+			}
+		}
+		catch (IOException e) {
+			// TODO: handle exception
+		}
 	}
 	
 }
