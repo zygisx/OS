18ac027d61d09db458ac02d7b0d7f6be7c315f52
Zygimantas Gatelis <zygis.gg@gmail.com>
2012-05-19 17:13:37 +0300

    implemented loader

--- src/processes/Loader.java
+++ src/processes/Loader.java
@@ -1,5 +1,9 @@
 package processes;
 
+import os.Kernel;
+import os.Resource;
+import machine.*;
+
 public class Loader extends Process{
 
 	
@@ -7,11 +11,37 @@ public class Loader extends Process{
 		super(id, parent, priority);
 		this.isSupervisorMode = true;	// true defines that it is system process
 		this.status = Status.BLOCKED;
-		this.missingResource = "loaderpackage"; // silly name
+		this.missingResource = "loaderstart"; 
 	}
 	
 	@Override
 	public void run() {
+		
+		// get memory from realMachine
+		Word[] mem = Realmachine.getVirtualMachineMemory();
+		
+		// get data and code from resources list
+		Resource dataRes = Kernel.getResources().getStartsWith("DATA");
+		String data = dataRes.getId().substring(4);
+		
+		Resource codeRes = Kernel.getResources().getStartsWith("CODE");
+		String code = codeRes.getId().substring(4);
+		
+		
+		
+		// load data and code to memory
+		Parser.loadData(data, mem);
+		Parser.loadCode(code, mem);
+		
+		// create resource 
+		Kernel.getResources().create(new Resource("loaderfinish", this.id));
+		
+		//destroy resource loaderstart
+		Kernel.getResources().destroy("loaderstart");
+		
+		//destroy data and code resources
+		Kernel.getResources().destroy(dataRes.getId());
+		Kernel.getResources().destroy(codeRes.getId());
 		
 	}
 }
