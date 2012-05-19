28b0ccee54fbcf3a8b84a0d5f33816461fdcc33b
Zygimantas Gatelis <zygis.gg@gmail.com>
2012-05-19 14:59:30 +0300

    first JCL process implementation, not fineshed

--- src/processes/JCL.java
+++ src/processes/JCL.java
@@ -1,5 +1,16 @@
 package processes;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+
+import exception.BadFileException;
+
+import machine.Parser;
+
+import os.Kernel;
+import os.Resource;
+
 public class JCL extends Process {
 
 	
@@ -8,11 +19,41 @@ public class JCL extends Process {
 		super(id, parent, priority);
 		this.isSupervisorMode = true;	// true defines that it is system process
 		this.status = Status.BLOCKED;
-		this.missingResource = "taskincache"; // task in cache
+		this.missingResource = "taskinsupmemory"; // task in cache
 	}
 
 	@Override
 	public void run() {
+		
+		switch (this.missingResource) {
+		
+			case "taskinsupmemory":
+				
+				Resource res = Kernel.getResources().getStartsWith("TASK");
+				if (res != null) {
+					 
+					try {
+						// ignoring first 4 chars
+						String[] task = Parser.validateTask(res.getId().substring(4));
+						
+						//create resource with task data
+						Kernel.getResources().create(new Resource("DATA" + task[0], this.id));
+						
+						// create resource with task source code
+						Kernel.getResources().create(new Resource("CODE" + task[1], this.id));
+						
+						
+					} catch (BadFileException e) {
+						Kernel.getResources().create(new Resource("jclerror " + e.getMessage(), this.id));
+					}
+					
+				}
+				
+				else {
+					// WTF?!?
+					return;
+				}
+		}
 		
 	}
 	
