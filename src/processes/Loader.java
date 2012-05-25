68e412894918770f3261d55350fc86c683c7e3e4
Zygimantas Gatelis <zygis.gg@gmail.com>
2012-05-25 16:52:23 +0300

    sysout deleted

--- src/processes/Loader.java
+++ src/processes/Loader.java
@@ -42,7 +42,6 @@ public class Loader extends Process{
 			String code = codeRes.getId().substring(4);
 			
 			
-			if (mem[0] == null) System.out.println("mem nullas");
 			// load data and code to memory
 			Parser.loadData(data, mem);
 			Parser.loadCode(code, mem);
