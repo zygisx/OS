dbdcbd337d3cee808aac0a05f56117442559adf7
Zygimantas Gatelis <zygis.gg@gmail.com>
2012-05-25 18:44:44 +0300

    added exception for processes

---
+++ src/exception/ProcessException.java
@@ -0,0 +1,11 @@
+package exception;
+
+public class ProcessException extends OSException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ProcessException(String message) {
+		super(message);
+	}
+
+}
