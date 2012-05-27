2804cae9dc68e81b28f35307daff83960d4e5c34
Zygimantas Gatelis <zygis.gg@gmail.com>
2012-05-27 23:36:13 +0300

    added processes for IO

--- src/processes/Input.java
+++ src/processes/Input.java
@@ -1,24 +1,24 @@
 package processes;
 
-import machine.Realmachine;
 import os.Kernel;
+import machine.Realmachine;
 import exception.ProcessException;
 import exception.VirtualMachineProgramException;
 
-public class Print extends Process {
+public class Input extends Process {
 
-	
-	public Print(String id, String parent, int priority) {
+
+	public Input(String id, String parent, int priority) {
 		super(id, parent, priority);
 		this.isSupervisorMode = true;	// true defines that it is system process
 		this.status = Status.BLOCKED;
-		this.missingResource = "printstart";
+		this.missingResource = "inputstart";
 	}
-	
+
 	@Override
 	public void run() throws ProcessException {
 		
-		String[] info = Kernel.getResources().get("printstart").getInfo().split(" ");
+		String[] info = Kernel.getResources().get("inputstart").getInfo().split(" ");
 		
 		int vmNum = Integer.parseInt(info[0]);
 		String command = info[1];			
@@ -33,7 +33,8 @@ public class Print extends Process {
 		Realmachine.getActiveVM().increaseIc();
 		
 		
-		Kernel.getResources().destroy("printstart");
-		
+		Kernel.getResources().destroy("inputstart");
+
 	}
+
 }
